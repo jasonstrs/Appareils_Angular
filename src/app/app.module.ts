6ee06cd2dab38060116cc83e1a748cb09d858610
jasonstrs <56001431+jasonstrs@users.noreply.github.com>
2020-02-25 11:10:08 +0100

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PremierCompoComponent } from './premier-compo/premier-compo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierCompoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/premier-compo/premier-compo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-premier-compo',
+  templateUrl: './premier-compo.component.html',
+  styleUrls: ['./premier-compo.component.scss']
+})
+export class PremierCompoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
